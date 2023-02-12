a5a8be87330a74d9a32bcd2bb9459e8660970374
Peter Christian Würdemann <christian.wuerdemann@gmail.com>
2023-02-12 12:31:02 +0100

    Initial Commit

---
+++ TranslationApp/TranslationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslationAppApp.swift
+//  TranslationApp
+//
+//  Created by Peter Christian Würdemann on 12.02.23.
+//
+
+import SwiftUI
+
+@main
+struct TranslationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
